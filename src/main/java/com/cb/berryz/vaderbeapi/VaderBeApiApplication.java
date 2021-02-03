14a3d9b49d740d976078a0fc1054be140ed34bc2
杉満寿央 <sugi@sugimanjuuounoMacBook-Pro.local>
2021-02-04 00:10:54 +0900

    first commit

---
+++ src/main/java/com/cb/berryz/vaderbeapi/VaderBeApiApplication.java
@@ -0,0 +1,13 @@
+package com.cb.berryz.vaderbeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaderBeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaderBeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cb/berryz/vaderbeapi/VaderBeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cb.berryz.vaderbeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaderBeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
