e231d5826162a1220e120f8154f5f1a450f3c06c
杉満寿央 <sugi@sugimanjuuounoMacBook-Pro.local>
2021-03-24 17:41:56 +0900

    部屋ステータス変更API

--- src/main/java/com/cb/berryz/vaderbeapi/application/controller/RoomController.java
+++ src/main/java/com/cb/berryz/vaderbeapi/application/controller/RoomController.java
@@ -1,6 +1,7 @@
 package com.cb.berryz.vaderbeapi.application.controller;
 
 import com.cb.berryz.vaderbeapi.application.request.CreateRoomRequest;
+import com.cb.berryz.vaderbeapi.application.request.RoomStatusUpdateRequest;
 import com.cb.berryz.vaderbeapi.domain.model.RoomModel;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
@@ -8,6 +9,8 @@ import io.swagger.annotations.ApiResponses;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
@@ -70,6 +73,36 @@ public class RoomController {
                 .setStatus("00")
                 .setPublicFlag(true)
                 .setChatDisplayType("00");
+
+    }
+
+    /**
+     * 部屋ステータス更新API
+     * @param request 更新用リクエスト
+     * @return 部屋情報
+     */
+    @ApiOperation(value = "部屋ステータス更新API",
+            notes = "部屋のステータスを更新しフロントへ返却する",
+            consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ApiResponses({
+            @ApiResponse(code = 500, message = "Internal Server Error")
+    })
+    @PutMapping
+    @MessageMapping("/status")
+    @SendTo("/game/match")
+    @ResponseBody
+    public RoomModel updateStatus(@RequestBody @NonNull final RoomStatusUpdateRequest request) {
+
+        // FIXME 一旦、固定値を返している
+        RoomModel roomModel = new RoomModel()
+                .setGameId(1000)
+                .setRoomId(1000)
+                .setRoomUrl("http://www.vader/room1234567890")
+                .setStatus("00")
+                .setPublicFlag(true)
+                .setChatDisplayType("01");
+
+        return roomModel;
 
     }
 }
---
+++ src/main/java/com/cb/berryz/vaderbeapi/application/request/RoomStatusUpdateRequest.java
@@ -0,0 +1,21 @@
+package com.cb.berryz.vaderbeapi.application.request;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
+@Accessors(chain = true)
+public class RoomStatusUpdateRequest {
+
+    @NotBlank
+    @ApiModelProperty(value = "部屋ID", example = "10000")
+    private int roomId;
+
+    @NotBlank
+    @ApiModelProperty(value = "ステータス", example = "01")
+    private String status;
+
+}
